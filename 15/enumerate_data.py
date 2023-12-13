cbe86919f7355ace234263d34850e27f2fc2cb3c
cyrustabatabai <cyrustabatabai-yazdi@ucsb.edu>
2023-12-12 16:24:42 -0800

    pybite 15

---
+++ 15/enumerate_data.py
@@ -0,0 +1,26 @@
+names = 'Julian Bob PyBites Dante Martin Rodolfo'.split()
+
+countries = 'Australia Spain Global Argentina USA Mexico'.split()
+
+
+def enumerate_names_countries():
+    """Outputs:
+       1. Julian     Australia
+       2. Bob        Spain
+       3. PyBites    Global
+       4. Dante      Argentina
+       5. Martin     USA
+       6. Rodolfo    Mexico"""
+
+
+    
+
+    result = []
+    for i,(name,country) in enumerate(zip(names,countries),1):
+        result.append(f"{i}. {name:<11}{country}")
+
+
+    print('\n'.join(result))
+
+
+
