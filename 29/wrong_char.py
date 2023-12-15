ac587c514e5dfb5219022999f1198bf1639460d7
cyrustabatabai <cyrustabatabai-yazdi@ucsb.edu>
2023-12-14 22:41:08 -0800

    29

---
+++ 29/wrong_char.py
@@ -0,0 +1,31 @@
+def get_index_different_char(chars):
+
+
+    counts = {True:0 ,False:0}
+
+
+
+    for char in chars:
+        counts[str(char).isalnum()] += 1
+
+
+
+    
+    if counts[True] == 1:
+        f = lambda x: x.isalnum()
+    else:
+        f = lambda x: not x.isalnum()
+
+
+
+    for i,char in enumerate(chars):
+        if f(str(char)):
+            return i
+
+
+
+
+
+
+
+
