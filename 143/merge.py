d36f2480ddcb6b71cd087360de2ec2860feff706
cyrustabatabai <cyrustabatabai-yazdi@ucsb.edu>
2023-12-20 14:15:46 -0800

    143

---
+++ 143/merge.py
@@ -0,0 +1,31 @@
+NOT_FOUND = "Not found"
+
+group1 = {'tim': 30, 'bob': 17, 'ana': 24}
+group2 = {'ana': 26, 'thomas': 64, 'helen': 26}
+group3 = {'brenda': 17, 'otto': 44, 'thomas': 46}
+
+
+def get_person_age(name):
+    """Look up name (case insensitive search) and return age.
+       If name in > 1 dict, return the match of the group with
+       greatest N (so group3 > group2 > group1)
+    """
+
+
+
+    groups = [group3,group2,group1]
+
+
+    for g in groups:
+        try:
+           age = g[name.lower()]
+        except:
+            pass
+        else:
+            return age
+
+
+
+
+
+    return NOT_FOUND
