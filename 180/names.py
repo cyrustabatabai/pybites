46902c87bacdb0f9929def9b29e85e460da1ca1c
cyrustabatabai <cyrustabatabai-yazdi@ucsb.edu>
2023-12-21 14:18:18 -0800

    180

---
+++ 180/names.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+from itertools import groupby
+
+# fake data from https://www.mockaroo.com
+data = """last_name,first_name,country_code
+Watsham,Husain,ID
+Harrold,Alphonso,BR
+Apdell,Margo,CN
+Tomblings,Deerdre,RU
+Wasielewski,Sula,ID
+Jeffry,Rudolph,TD
+Brenston,Luke,SE
+Parrett,Ines,CN
+Braunle,Kermit,PL
+Halbard,Davie,CN"""
+
+
+def group_names_by_country(data: str = data) -> defaultdict:
+    countries = defaultdict(list)
+
+
+    for line in data.splitlines()[1:]:
+        line = line.strip()
+
+        last,first,country = line.split(',')
+        countries[country].append(f"{first} {last}")
+
+
+    return countries
+        
+
+
